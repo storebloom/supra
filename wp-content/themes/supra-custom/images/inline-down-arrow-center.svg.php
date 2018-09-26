3a6ff20636a422935b87ecb89ec0bbef5ae081f8
Scott Adrian <scott@storebloom.com>
2018-09-25 22:26:43 -0700

    logistic styling

---
+++ wp-content/themes/supra-custom/images/inline-down-arrow-center.svg.php
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<svg width="62px" height="276px" viewBox="0 0 62 276" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 51.2 (57519) - http://www.bohemiancoding.com/sketch -->
+    <title>Group 5</title>
+    <desc>Created with Sketch.</desc>
+    <defs></defs>
+    <g id="Desktop" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="5.0---Logistics---Desktop" transform="translate(-689.000000, -1104.000000)" stroke="#000000">
+            <g id="Group-5" transform="translate(690.000000, 1105.000000)">
+                <g id="lkjlkj" transform="translate(23.000000, 0.000000)" stroke-linecap="square">
+                    <path d="M7,10 L7,103" id="Line-2"></path>
+                    <path d="M13,20 L13,103" id="Line-2"></path>
+                    <path d="M1,0 L1,103" id="Line-2"></path>
+                </g>
+                <g id="Scroll-Aroown" transform="translate(0.000000, 103.000000)">
+                    <circle id="Oval" fill="#F5F5F5" cx="30" cy="30" r="30"></circle>
+                    <g id="Icon---Arrow-Down" transform="translate(21.000000, 28.000000)" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                        <polyline id="Stroke-2" points="0 0 9 9 18 0"></polyline>
+                    </g>
+                </g>
+                <path d="M30,164 L30,264" id="Line-2" stroke-linecap="square"></path>
+                <path d="M36,164 L36,274" id="Line-2" stroke-linecap="square"></path>
+                <path d="M24,164 L24,254" id="Line-2" stroke-linecap="square"></path>
+            </g>
+        </g>
+    </g>
+</svg>
